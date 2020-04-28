08e1696294952a68dd5903e8f508b3ebb66558b2
KeilaAlejandra <castrokeila08@gmail.com>
2020-04-27 23:01:11 -0500

    Practica models and controllers

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Models;
+using MVCLaboratorio.Utilerias;
+
+
+namespace MVCLaboratorio.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //mostrar un formulario para crear el video
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Video datosVideo)
+        {
+            //Guarda el Video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@Nombre",datosVideo.Nombre));
+            parametros.Add(new SqlParameter("@Url",datosVideo.Url));
+            parametros.Add(new SqlParameter("@FechaPublicacion",datosVideo.FechaPublicacion));
+            
+            BaseHelper.ejecutarSentencia("sp_Video_Insertar", CommandType.StoredProcedure,parametros);
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Views/Home/Index.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>VIDEOS Gpo42</title>
+</head>
+<body>
+    <img src="../../Content/images/logo-facpya.png" />
+    <h1> BIENVENIDO A MI SITIO WEB DE VIDEOS
+    </h1>
+    <a href="/Video/Index">Ir a Videos</a>
+</body>
+</html>
